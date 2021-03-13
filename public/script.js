ce4eacfc1bf61292c53c24017b933b50bb677341
Jon Michael Ossola <jm.ossola24@gmail.com>
2021-03-12 20:09:48 -0700

    adding stuff

--- public/script.js
+++ public/script.js
@@ -2,7 +2,9 @@ function onFriendTextInput() {
   console.log("hello world test");
   // hardcoded query string parameter for now
   const userId = 1;
-  fetch(`https://afternoon-tor-78552.herokuapp.com?userId=${userId}`)
+  fetch(
+    `https://afternoon-tor-78552.herokuapp.com/getChatMates?userId=${userId}`
+  )
     .then(res => res.json())
     .then(data => {
       console.log(data);
